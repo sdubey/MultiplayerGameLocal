4d4d90b666a39a84939ca3f21ac281676766bc71
sonal dubey <sonal2404@gmail.com>
2013-03-19 12:24:47 -0700

    adding my local code

---
+++ SDU_Server_Connectivity/src/sfsu/edu/interfaces/MultiplayerAccessPlantsDB.java
@@ -0,0 +1,9 @@
+package sfsu.edu.interfaces;
+
+public interface MultiplayerAccessPlantsDB {
+
+	//This inrterface has the signature of all the functions/information 
+	//required by the server regarding plants
+	
+	
+}
